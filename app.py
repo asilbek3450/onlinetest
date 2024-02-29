1cbcbd6fec022ab4ac731d10c7f9bf45303851cf
asilbek <asilbekmirolimov@gmail.com>
2024-02-29 18:30:45 +0500

    changes

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
 from aiogram import executor
 
-from loader import dp
+from loader import dp, db_manager
 import middlewares, filters, handlers
 from utils.notify_admins import on_startup_notify
 from utils.set_bot_commands import set_default_commands
 
 
 async def on_startup(dispatcher):
-    # Birlamchi komandalar (/star va /help)
+    db_manager.delete_table("users")
+    db_manager.create_table()
     await set_default_commands(dispatcher)
-
-    # Bot ishga tushgani haqida adminga xabar berish
     await on_startup_notify(dispatcher)
 
 
--- utils/db_api/database.py
+++ utils/db_api/database.py
@@ -7,7 +7,62 @@ class DatabaseManager:
         self.cursor = self.conn.cursor()
 
     def create_table(self):
-        pass
+        self.cursor.execute(
+            """
+            CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY,
+                full_name TEXT,
+                user_id INTEGER
+            )
+            """
+        )
 
+        self.cursor.execute(
+            """
+            CREATE TABLE IF NOT EXISTS tests (
+                id INTEGER PRIMARY KEY,
+                teacher TEXT,
+                subject TEXT,
+                test_number INTEGER PRIMARY KEY,
+                quantity_questions INTEGER,
+                all_answers TEXT,
+                total_score INTEGER,
+                status BOOLEAN,               
+            )
+            """
+        )
+
+        self.cursor.execute(
+            """
+                CREATE TABLE IF NOT EXISTS user_test_connection (
+                id INTEGER PRIMARY KEY,
+                user_id INTEGER,
+                test_id INTEGER,
+                FOREIGN KEY (user_id) REFERENCES users (id),
+                FOREIGN KEY (test_id) REFERENCES tests (id),
+                sent_answers TEXT,
+                correct_answers TEXT,
+                total_score INTEGER,
+                persentage INTEGER
+            )
+            """
+        )
+
+        self.conn.commit()
+
+    def delete_table(self, table_name):
+        self.cursor.execute(f"DROP TABLE {table_name}")
+        self.conn.commit()
+
+    def add_user(self, full_name, user_id):
+        self.cursor.execute(
+            f"INSERT INTO users (full_name, user_id) VALUES ('{full_name}', {user_id})"
+        )
+        self.conn.commit()
+
+    def add_test(self, teacher, subject, test_number, quantity_questions, all_answers, total_score, status):
+        self.cursor.execute(
+            f"INSERT INTO tests (teacher, subject, test_number, quantity_questions, all_answers, total_score, status) VALUES ('{teacher}', '{subject}', {test_number}, {quantity_questions}, '{all_answers}', {total_score}, {status})"
+        )
         self.conn.commit()
 
