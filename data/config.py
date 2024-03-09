2bb1a771e3abb3e2462b1661d996e27d043db1c5
asilbek <asilbekmirolimov@gmail.com>
2024-03-09 14:11:48 +0500

    initial webhook

--- data/config.py
+++ data/config.py
@@ -5,7 +5,7 @@ env.read_env()
 
 BOT_TOKEN = env.str("BOT_TOKEN")
 ADMINS = env.list("ADMINS")
-
+DOMAIN = env.str("DOMAIN")
 
 CHANNEL_URL = env.str("CHANNEL_URL")
 CHANNEL_ID = env.str("CHANNEL_ID")
